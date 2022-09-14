257af0c5202840c457ed3311bc99e4e8866d441f
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-09-14 09:37:32 -0300

    form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,23 @@
 const Contact = () => {
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+  }
+
   return (
-    <>
-      <h1>Contact</h1>
-    </>
+    <form onSubmit={handleSubmit} className="flex column">
+      <label>
+        Name
+        <input type="text" placeholder="digite seu nome..." required />
+      </label>
+      <label>
+        email
+        <input type="email" placeholder="digite seu email..." required />
+      </label>
+      <label htmlFor="message">Mensagem</label>
+      <textarea id="message" rows={10} type="text" placeholder="deixe sua menssagem..." required />
+      <input className="btn" type="submit" value="enviar" />
+    </form>
   )
 }
 
